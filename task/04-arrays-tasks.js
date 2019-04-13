5e1b0ed39b6e857d76428d78ae89515383b02929
Yaroslav Mescheryakov <migyaroslav@gmail.com>
2019-04-14 00:16:29 +0300

    feat: complete array tasks #6

--- task/04-arrays-tasks.js
+++ task/04-arrays-tasks.js
@@ -110,7 +110,7 @@ function getArrayOfStrings(arr) {
  *    [ false, 0, NaN, '', undefined ]   => [ ]
  */
 function removeFalsyValues(arr) {
-  throw new Error('Not implemented');
+  return arr.filter(v => !!v);
 }
 
 /**
