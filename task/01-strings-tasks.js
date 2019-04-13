fcd62db201a59cc4aa6cc41545a450124fc74e63
Yaroslav Mescheryakov <migyaroslav@gmail.com>
2019-04-13 18:54:24 +0300

    feat: complete string tasks #12

--- task/01-strings-tasks.js
+++ task/01-strings-tasks.js
@@ -202,7 +202,9 @@ function extractEmails(str) {
  *
  */
 function getRectangleString(width, height) {
-  throw new Error('Not implemented');
+  return String.fromCharCode(9484) + String.fromCharCode(9472).repeat(width - 2) + String.fromCharCode(9488) + "\n" +
+    (String.fromCharCode(9474) + " ".repeat(width - 2) + String.fromCharCode(9474) + "\n").repeat(height - 2) +
+    String.fromCharCode(9492) + String.fromCharCode(9472).repeat(width - 2) + String.fromCharCode(9496) + "\n";
 }
 
 
