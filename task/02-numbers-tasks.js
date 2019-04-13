8bc60f338eeb374ccbbef912cd60c5953cea2ad2
Yaroslav Mescheryakov <migyaroslav@gmail.com>
2019-04-13 16:17:38 +0300

    feat: complete number tasks #7

--- task/02-numbers-tasks.js
+++ task/02-numbers-tasks.js
@@ -130,7 +130,8 @@ function getAngleBetweenVectors(x1, y1, x2, y2) {
  *     0     => 0
  */
 function getLastDigit(value) {
-  throw new Error('Not implemented');
+  const str = [value][0].toString();
+  return Number(str[str.length - 1]);
 }
 
 
