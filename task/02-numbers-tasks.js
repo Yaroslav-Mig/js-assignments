b6420541c9381f6a177a6488d6753b4f5e5a544c
Yaroslav Mescheryakov <migyaroslav@gmail.com>
2019-04-13 13:37:41 +0300

    feat: complete number tasks #2-3

--- task/02-numbers-tasks.js
+++ task/02-numbers-tasks.js
@@ -38,7 +38,8 @@ function getRectangleArea(width, height) {
  *   0    => 0
  */
 function getCicleCircumference(radius) {
-  throw new Error('Not implemented');
+  const PI = Math.PI;
+  return 2 * PI * radius;
 }
 
 /**
@@ -54,7 +55,7 @@ function getCicleCircumference(radius) {
  *  -3, 3  => 0
  */
 function getAverage(value1, value2) {
-  throw new Error('Not implemented');
+  return value1 / 2 + value2 / 2;
 }
 
 /**
